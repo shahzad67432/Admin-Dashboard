76487ab8caf1126756557da6115cc1842e6b1cdd
shahzad67432 <shaa1891640@gmail.com>
2024-01-27 06:00:28 +0500

    adding the context into the Sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { useStateContext } from "../context/ContextProoovider";
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+        <Button/>
+    </div>
   )
 }
+
+function Button() {
+  const {activeMenu, setActiveMenu} = useStateContext();
+  return ( <>
+    <div className= {!activeMenu}>
+      <button onClick={()=> setActiveMenu(true)}>
+        click me 
+      </button>
+    </div>
+  </> );
+}
+
 
 export default Navbar
--- src/context/ContextProoovider.js
+++ src/context/ContextProoovider.js
@@ -13,10 +13,11 @@ const initialState = {
 export const ContextProvider = ({children}) => {
     const [activeMenu, setActiveMenu] = useState(true)
     return (
-        <StateContext.Provider value={{activeMenu}}>
+        <StateContext.Provider value={{activeMenu, setActiveMenu}}>
             {children}
         </StateContext.Provider>
     )
 }
 
+//  custom-hook that conntain a hook inside 
 export const useStateContext = () =>  useContext(StateContext)
